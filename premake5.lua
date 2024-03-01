24fb3669b51656bcbb1c155cc1443cd615f62870
RxZoid24 <umarlucio@gmail.com>
2024-02-29 23:28:13 -0300

    Multiple render targets support

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	defines{
+        "_CRT_SECURE_NO_WARNINGS",
+        "YAML_CPP_STATIC_DEFINE"
+    }
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
